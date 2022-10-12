6b7ea2bbf2196b310de61bf00a6126c93c98d75a
Weslley Reis <weslley.reis@ymail.com>
2022-10-12 18:30:00 -0300

    SemLayout

---
+++ faq.js
@@ -0,0 +1,11 @@
+const question = document.querySelectorAll(".question-item");
+const answer = document.querySelectorAll(".answer-item");
+// const box = document.querySelector(".box");
+const faq = document.querySelector(".faq");
+
+function selecionar(b) {
+  var selecionado = answer.item(b);
+  var questao = question.item(b);
+  questao.classList.toggle("question-item-active");
+  selecionado.classList.toggle("answer-item-display");
+}
